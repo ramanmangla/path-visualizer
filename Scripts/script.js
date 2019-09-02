1ac04970592799efe3ca09b1864335a7ebf5818d
Raman Mangla <ramanmangla7@gmail.com>
2019-09-02 15:43:47 +0530

    Added onclick events for selecting grid cells

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,110 +1,51 @@
-// Graph Class
-class Graph {
-	constructor(vertices, edges, adjacencyList){
-		this.vertices = vertices;
-		this.edges = edges;
-		this.adjacencyList = adjacencyList;
-	}
-}
+// cell size in pixels
+let columns;
+let rows;
+let cellSize = 30;
+// Accounting for border width
+cellSize = cellSize + 1;
+let grid = $(".grid");
 
-// Vertex Class
-class Vertex {
-	/*
-		Initialzes vertex number 'num'.
-		Initially, all vertices are colored "white", have no parent, and are not walls
-		They are also "infinitley" away from the source, as they haven't been discovered
-	*/
-	constructor(num){
-		this.num = num;
-		// All vertices are 
-		this.distance = Number.MAX_SAFE_INTEGER;
-		// Initially, all vertices are white
-		this.color = 'w';
-		this.parent = null;
-		this.isWall = false;
-	}
-}
+const gridGeneration = callback => {
+  columns = Math.floor(grid.width() / cellSize);
+  rows = Math.floor(grid.height() / cellSize);
 
-// A Min PriorityQueue Class
-class PriorityQueue {
-	constructor(){
-		// Array used to represent the queue
-		this.baseArray = [];
-	}
+  grid.css("grid-template-columns", "repeat(" + columns + ", 1fr)");
+  grid.css("grid-template-rows", "repeat(" + rows + ", 1fr)");
 
-	getMinimum(){
-		return baseArray[0];
-	}
+  // Clearing the grid
+  grid.html("");
 
-	minHeapify(index){
-		// Get the index of the left and right child of index node
-		const left = 2*index + 1;
-		const right = 2*index + 2;
-		let smallest = 0;
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < columns; col++) {
+      grid.append('<div class="grid-cell"></div>');
+    }
+  }
 
-		/*
-			Figure out who the smallest is among:
-			baseArray[left], baseArray[right], baseArray[index]
-		*/
-		if(left <= this.baseArray.length && 
-			this.baseArray[left] < this.baseArray[index]){
-			smallest = left;
-		}else{
-			smallest = index;
-		}
+  callback();
+};
 
-		if(right <= this.baseArray.length && 
-			this.baseArray[right] < this.baseArray[smallest]){
-			smallest = right;
-		}
+const setClickEvents = () => {
+  $(".grid-cell").on("click", event => {
+    // $(this).addClass("grid-cell-active");
+    $(event.target).addClass("grid-cell-active");
 
-		/*
-			If baseArray[index] wasn't smallest, swap it with the 
-			smallest index. Then, minHeapify at smallest index
-		*/
-		if(smallest != index){
-			const temp = this.baseArray[index];
-			this.baseArray[index] = this.baseArray[smallest];
-			this.baseArray[smallest] = temp;
-			this.minHeapify(smallest);
-		}
-	}
+    console.log(event.target);
+    // $(this).css("background");
+    let leftPosition = event.pageX - grid.offset().left;
+    let topPosition = event.pageY - grid.offset().top;
 
-	extractMin(){
-		if(this.baseArray.length >= 1){
-			const min = this.baseArray[0];
-			this.baseArray[0] = this.baseArray[this.baseArray.length - 1];
-			// Remove the last element.
-			this.baseArray.pop();
-			this.minHeapify(0);
-			return min;
-		}
-	}
+    let colIndex = Math.floor(leftPosition / cellSize);
+    let rowIndex = Math.floor(topPosition / cellSize);
 
-	decreaseKey(index, key){
-		if(key <= this.baseArray[key].distance){
-			this.baseArray[index].distance = key;
-			let parentIndex = Math.floor(index / 2);
-			while(index > 0 && this.baseArray[parentIndex] > A[index]){
-				// Swap A[index] with A[parentIndex]
-				let temp = A[index];
-				A[index] = A[parentIndex];
-				A[parentIndex] = temp;
-				// Set index to parentIndex
-				index = parentIndex;
-				// Recalculate parentIndex
-				parentIndex = Math.floor(index / 2);
-			}
-		}
-	}
+    // console.log($(this).offset().left);
+  });
+};
 
-	insert(vertex){
-		this.baseArray.push(vertex);
-		const actualKey = vertex.distance;
-		/* Set vertex distnace to the maximum to ensure key actually decreases
-		 See implementation of decreaseKey for details */
-		vertex.distance = Number.MAX_SAFE_INTEGER;
-		// Decrease this vertex's key to actual key
-		this.decreaseKey(this.baseArray.length - 1, actualKey);
-	}
-}
+$(() => {
+  gridGeneration(setClickEvents);
+});
+
+$(window).resize(() => {
+  gridGeneration(setClickEvents);
+});
