9ce76380d5ebefbbf3fe7b100bc4a0a8e33ec89a
Raman Mangla <ramanmangla7@gmail.com>
2019-09-04 04:26:07 +0530

    Modified start and end point initiaization and added function to transform from position to indices

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,6 +12,10 @@ let cellSize = 25;
 // Accounting for grid gap
 cellSize = cellSize + 4;
 
+// Flags for moving start and end points
+let startMove = false;
+let endMove = false;
+
 let grid = $(".grid");
 
 const setCellClass = (row, col, className) => {
@@ -19,15 +23,33 @@ const setCellClass = (row, col, className) => {
   $(".grid-cell:nth-child(" + index + ")").addClass(className);
 };
 
+const positionToIndices = event => {
+  // [row, col];
+  let indices = [];
+
+  let leftPosition = event.pageX - grid.offset().left;
+  let topPosition = event.pageY - grid.offset().top;
+
+  // Indices for graph
+  indices[0] = Math.floor(topPosition / cellSize);
+  indices[1] = Math.floor(leftPosition / cellSize);
+
+  return indices;
+};
+
 const gridGeneration = callback => {
   columns = Math.floor(grid.width() / cellSize);
   rows = Math.floor(grid.height() / cellSize);
 
-  // Random start and end points generation
-  startPoint[0] = Math.floor(Math.random() * rows);
-  endPoint[0] = Math.floor(Math.random() * rows);
-  startPoint[1] = Math.floor(Math.random() * (columns / 2));
-  endPoint[1] = Math.floor((columns / 2) * (Math.random() + 1));
+  // // Random start and end points generation
+  // startPoint[0] = Math.floor(Math.random() * rows);
+  // endPoint[0] = Math.floor(Math.random() * rows);
+  // startPoint[1] = Math.floor(Math.random() * (columns / 2));
+  // endPoint[1] = Math.floor((columns / 2) * (Math.random() + 1));
+  startPoint[0] = Math.floor(rows / 2);
+  endPoint[0] = Math.floor(rows / 2);
+  startPoint[1] = Math.floor(columns / 3);
+  endPoint[1] = Math.floor((columns / 3) * 2);
 
   grid.css("grid-template-columns", "repeat(" + columns + ", 1fr)");
   grid.css("grid-template-rows", "repeat(" + rows + ", 1fr)");
@@ -46,33 +68,34 @@ const gridGeneration = callback => {
 
 const setClickEvents = () => {
   $(".grid-cell").on("mouseover", event => {
-    if (mousePressed === true) {
+    if (
+      mousePressed === true &&
+      !$(event.target).hasClass("grid-cell-start") &&
+      !$(event.target).hasClass("grid-cell-end")
+    ) {
       $(event.target).addClass("grid-cell-active");
     }
-
-    let leftPosition = event.pageX - grid.offset().left;
-    let topPosition = event.pageY - grid.offset().top;
-
-    // Indices for graph
-    let colIndex = Math.floor(leftPosition / cellSize);
-    let rowIndex = Math.floor(topPosition / cellSize);
   });
 
   $(".grid-cell").on("mousedown", event => {
-    $(event.target).addClass("grid-cell-active");
-
-    let leftPosition = event.pageX - grid.offset().left;
-    let topPosition = event.pageY - grid.offset().top;
-
-    // Indices for graph
-    let colIndex = Math.floor(leftPosition / cellSize);
-    let rowIndex = Math.floor(topPosition / cellSize);
+    if (
+      mousePressed === true &&
+      !$(event.target).hasClass("grid-cell-start") &&
+      !$(event.target).hasClass("grid-cell-end")
+    ) {
+      $(event.target).addClass("grid-cell-active");
+    } else if ($(event.target).hasClass("grid-cell-start")) {
+      startMove = true;
+    } else {
+      endMove = true;
+    }
   });
 
   $("#clearButton").on("click", () => {
     $(".grid-cell").removeClass("grid-cell-active");
   });
 
+  // Set classes for random start and end points
   setCellClass(startPoint[0], startPoint[1], "grid-cell-start");
   setCellClass(endPoint[0], endPoint[1], "grid-cell-end");
 };
@@ -86,12 +109,14 @@ $(window).resize(() => {
 });
 
 $(document).on("mousedown", event => {
+  // Left click
   if (event.button === 0) {
     mousePressed = true;
   }
 });
 
 $(document).on("mouseup", event => {
+  // Left click
   if (event.button === 0) {
     mousePressed = false;
   }
