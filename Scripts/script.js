70f8f310986c4b9e870e7f4fe63c17299d3f926c
Raman Mangla <ramanmangla7@gmail.com>
2019-09-04 03:34:45 +0530

    Added code for random start and end point generation and function to set grid cell class using indices

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,16 +1,33 @@
 // Mouse press state
 let mousePressed = false;
-// cell size in pixels
 let columns;
 let rows;
+
+// Coordinates [row, col]
+let startPoint = [];
+let endPoint = [];
+
+// cell size in pixels
 let cellSize = 25;
 // Accounting for grid gap
 cellSize = cellSize + 4;
+
 let grid = $(".grid");
+
+const setCellClass = (row, col, className) => {
+  let index = row * columns + (col + 1);
+  $(".grid-cell:nth-child(" + index + ")").addClass(className);
+};
 
 const gridGeneration = callback => {
   columns = Math.floor(grid.width() / cellSize);
   rows = Math.floor(grid.height() / cellSize);
+
+  // Random start and end points generation
+  startPoint[0] = Math.floor(Math.random() * rows);
+  endPoint[0] = Math.floor(Math.random() * rows);
+  startPoint[1] = Math.floor(Math.random() * (columns / 2));
+  endPoint[1] = Math.floor((columns / 2) * (Math.random() + 1));
 
   grid.css("grid-template-columns", "repeat(" + columns + ", 1fr)");
   grid.css("grid-template-rows", "repeat(" + rows + ", 1fr)");
@@ -33,10 +50,10 @@ const setClickEvents = () => {
       $(event.target).addClass("grid-cell-active");
     }
 
-    console.log(event.target);
-
     let leftPosition = event.pageX - grid.offset().left;
     let topPosition = event.pageY - grid.offset().top;
+
+    // Indices for graph
     let colIndex = Math.floor(leftPosition / cellSize);
     let rowIndex = Math.floor(topPosition / cellSize);
   });
@@ -44,10 +61,10 @@ const setClickEvents = () => {
   $(".grid-cell").on("mousedown", event => {
     $(event.target).addClass("grid-cell-active");
 
-    console.log(event.target);
-
     let leftPosition = event.pageX - grid.offset().left;
     let topPosition = event.pageY - grid.offset().top;
+
+    // Indices for graph
     let colIndex = Math.floor(leftPosition / cellSize);
     let rowIndex = Math.floor(topPosition / cellSize);
   });
@@ -55,6 +72,9 @@ const setClickEvents = () => {
   $("#clearButton").on("click", () => {
     $(".grid-cell").removeClass("grid-cell-active");
   });
+
+  setCellClass(startPoint[0], startPoint[1], "grid-cell-start");
+  setCellClass(endPoint[0], endPoint[1], "grid-cell-end");
 };
 
 $(() => {
